d5f28cafb94b3b3d9d061487aa6c2b493de915eb
James Lacroix <Lacroix.James@icloud.com>
2022-09-29 11:01:42 -0700

    Update tmux.conf, add collection script

---
+++ collect.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp -i ~/.vimrc ./vimrc 
+cp -i ~/.config/vimrc-plugins ./vimrc-plugins
+cp -i ~/.config/vimrc-additions ./vimrc-additions 
+
+cp -i ~/.bashrc ./bashrc
+cp -i ~/.bash_aliases ./bash_aliases
+cp -i ~/.tmux.conf ./tmux.conf
